2cd13fde1ed35de0b701a8130fb0fa09c4e78701
BioAimie <aimie_faucett@iSAOS>
2017-05-22 13:03:15 -0600

    anna's updates to dateManip to fix limit calc

--- R/addStatsToSparseHandledData.R
+++ R/addStatsToSparseHandledData.R
@@ -10,6 +10,8 @@
 #' @param limitSide a character string, default of upper, that indicates whether the limit should be +X from the mean (upper), -X from the mean (lower), or +/-X from the mean (two.sided)
 #' @param altUL a numeric, default of 0.001, that indicates a default limit if the altUL > limitFactor*sd(X)
 #' @param altLL a numeric, default of 0, that indicates a default limit if the altLL < limitFactor*sd(X)
+#' @param dategroup is a string indicating which dategroup the input sparseRateFrame has, i.e. "Week", "Month", "Quarter"
+#' @param keepPeriods is an integer indicating the number of dategroup periods that will going into calculating the mean/standard deviation (i.e. 2 would mean 2 weeks if you have week dategroups)
 #' @return a data frame that contains the orginal columns in sparseRateFrame, as well as an average for each of the ratePartitionVec combinations. It may also have limits if limit parameters are specified.
 #' @author Aimie Faucett
 #' @details
@@ -19,21 +21,43 @@
 #' altUL and altLL allow the user to specify an alternate limit that can be applied instead of the limitFactor.
 #' @export
 
-addStatsToSparseHandledData <- function(sparseRateFrame, ratePartitionVec, ignorePeriods = 0, returnLimits = FALSE, limitFactor = 3, limitSide = 'upper', altUL = 0.001, altLL = 0.00) {
+addStatsToSparseHandledData <- function(sparseRateFrame, ratePartitionVec, ignorePeriods = 0, returnLimits = FALSE, limitFactor = 3, limitSide = 'upper', altUL = 0.001, altLL = 0.00, keepPeriods) {
 
   if(length(ratePartitionVec) == 1) {
-
     sparseRateFrame[,'combocat'] <- sparseRateFrame[,ratePartitionVec]
   } else {
-
     sparseRateFrame[,'combocat'] <- do.call(paste, c(sparseRateFrame[,ratePartitionVec], sep=','))
   }
 
   comboCats <- as.character(unique(sparseRateFrame[,'combocat']))
-  avgFrame <- as.data.frame(do.call(rbind, lapply(1:length(comboCats), function(x) cbind(combocat = comboCats[x], Avg = mean(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ][with(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ], order(DateGroup)), 'Rate'][1:(length(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x],'DateGroup']) - ignorePeriods)], na.rm=TRUE)))))
-  avgFrame[,'Avg'] <- as.numeric(as.character(avgFrame[,'Avg']))
-  avgFrame[is.nan(avgFrame[,'Avg']),'Avg'] <- NA
-  sparseRateFrame <- merge(sparseRateFrame, avgFrame, by='combocat')
+
+  if(keepPeriods == 0){
+    avgFrame <- as.data.frame(do.call(rbind, lapply(1:length(comboCats), function(x) cbind(combocat = comboCats[x], Avg = mean(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ][with(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ], order(DateGroup)), 'Rate'][1:(length(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x],'DateGroup']) - ignorePeriods)], na.rm=TRUE)))))
+    avgFrame[,'Avg'] <- as.numeric(as.character(avgFrame[,'Avg']))
+    avgFrame[is.nan(avgFrame[,'Avg']),'Avg'] <- NA
+    sparseRateFrame <- merge(sparseRateFrame, avgFrame, by='combocat')
+
+  }else{
+    comboCat.list <- vector(mode="list")
+    for(x in comboCats) {
+
+      combocat.df <- sparseRateFrame[which(sparseRateFrame$combocat == x), ]
+      combocat.df <- combocat.df[order(combocat.df$DateGroup), ]
+
+      unique.dates <- unique(combocat.df$DateGroup)
+      start.index <- min(which(combocat.df$DateGroup == unique.dates[keepPeriods+1] ))
+
+      averages <- unlist(lapply(combocat.df$DateGroup[start.index:nrow(combocat.df)], function(j)mean(combocat.df[which(combocat.df$DateGroup %in% unique.dates[(which(unique.dates == j)-keepPeriods):(which(unique.dates == j)-ignorePeriods)]), 'Rate'], na.rm=TRUE)))
+      averagecat <-  paste( combocat.df$DateGroup[start.index:nrow(combocat.df)] , ",", x, sep="")
+      comboCat.list[[x]] <- cbind(dateGroupcombocat=averagecat, Avg=averages)
+    }
+    avgFrame <- as.data.frame(do.call(rbind, comboCat.list))
+    avgFrame[,'Avg'] <- as.numeric(as.character(avgFrame[,'Avg']))
+    avgFrame[is.nan(avgFrame[,'Avg']),'Avg'] <- NA
+    sparseRateFrame$dateGroupcombocat <- paste(sparseRateFrame$DateGroup, ",", sparseRateFrame$combocat, sep="")
+    sparseRateFrame.original <- sparseRateFrame
+    sparseRateFrame <- unique(merge(sparseRateFrame, avgFrame, by='dateGroupcombocat'))
+  }
 
   if(returnLimits == FALSE) {
 
@@ -41,9 +65,28 @@ addStatsToSparseHandledData <- function(sparseRateFrame, ratePartitionVec, ignor
     keepCols <- colnames(sparseRateFrame)[!(colnames(sparseRateFrame) %in% colnames(sparseRateFrame)[grep('combocat', colnames(sparseRateFrame))])]
     return(sparseRateFrame[,keepCols])
   } else {
+    if(keepPeriods == 0){
+      sdFrame <- as.data.frame(do.call(rbind, lapply(1:length(comboCats), function(x) cbind(combocat = comboCats[x], Sdev = sd(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ][with(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ], order(DateGroup)), 'Rate'][1:(length(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x],'Rate']) - ignorePeriods)], na.rm=TRUE)))))
+      sparseRateFrame <- merge(sparseRateFrame, sdFrame, by='combocat')
+    } else {
+      comboCat.list <- vector(mode="list")
+      for(x in comboCats){
 
-    sdFrame <- as.data.frame(do.call(rbind, lapply(1:length(comboCats), function(x) cbind(combocat = comboCats[x], Sdev = sd(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ][with(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x], ], order(DateGroup)), 'Rate'][1:(length(sparseRateFrame[sparseRateFrame[,'combocat'] == comboCats[x],'Rate']) - ignorePeriods)], na.rm=TRUE)))))
-    sparseRateFrame <- merge(sparseRateFrame, sdFrame, by='combocat')
+        combocat.df <- sparseRateFrame.original[which(sparseRateFrame.original$combocat == x), ]
+        combocat.df <- combocat.df[order(combocat.df$DateGroup), ]
+
+        unique.dates <- unique(combocat.df$DateGroup)
+        start.index <- min(which(combocat.df$DateGroup == unique.dates[keepPeriods+1] ))
+
+        sdevs <- unlist(lapply(combocat.df$DateGroup[start.index:nrow(combocat.df)], function(j)sd(combocat.df[which(combocat.df$DateGroup %in% unique.dates[(which(unique.dates == j)-keepPeriods):(which(unique.dates == j)-ignorePeriods)]), 'Rate'], na.rm=TRUE)))
+        sdevcat <-  paste( combocat.df$DateGroup[start.index:nrow(combocat.df)] , ",", x, sep="")
+        comboCat.list[[x]] <- cbind(dateGroupcombocat=sdevcat, Sdev=sdevs)
+      }
+
+      sdFrame <- as.data.frame(do.call(rbind, comboCat.list))
+      sparseRateFrame$dateGroupcombocat <- paste(sparseRateFrame$DateGroup, ",", sparseRateFrame$combocat, sep="")
+      sparseRateFrame <- unique(merge(sparseRateFrame, sdFrame, by='dateGroupcombocat'))
+    }
     sparseRateFrame[,'Sdev'] <- as.numeric(as.character(sparseRateFrame[,'Sdev']))
 
     if(limitSide == 'upper') {
@@ -68,7 +111,6 @@ addStatsToSparseHandledData <- function(sparseRateFrame, ratePartitionVec, ignor
       stop("The limitSide parameter has been specified incorrectly... the function take three options: 'upper', 'lower', or 'two.sided'.")
     }
 
-    sparseRateFrame <- sparseRateFrame[!(is.na(sparseRateFrame[,'Avg'])), ]
     keepCols <- colnames(sparseRateFrame)[!(colnames(sparseRateFrame) %in% colnames(sparseRateFrame)[grep('combocat', colnames(sparseRateFrame))])]
     return(sparseRateFrame[,keepCols])
   }
--- R/findStartDate.R
+++ R/findStartDate.R
@@ -6,6 +6,7 @@
 #' @param yearSubGroup is a sub-group of a year, i.e. 'Week', 'Month', or 'Quarter'
 #' @param timeUnitsToKeep is a numeric value indicating how many of DateGroup entries to keep (incremented by 1 backwards from today's DateGroup)
 #' @param rollPeriods is a numeric value indicating how many additional periods should be included in case the user wants to find a rolling sum, the default is 0.
+#' @param keepPeriods is a numeric value indicating how many periods to keep such that the limit at calculation is the same as what it would have been when the period was current.
 #' @return a character string DateGroup at timeUnitsToKeep + rollPeriods prior (e.g. 2016-01 if today's DateGroup is 2016-10 and timeUnitsToKeep = 9 and rollPeriods = 0)
 #' @author Aimie Faucett
 #' @details
@@ -14,8 +15,10 @@
 #' rollPeriods prior to the most current DateGroup.
 #' @export
 
-findStartDate <- function(msCalFrame, yearSubGroup, timeUnitsToKeep, rollPeriods = 0) {
+findStartDate <- function(msCalFrame, yearSubGroup, timeUnitsToKeep, rollPeriods = 0 , keepPeriods) {
 
+
+  timeUnitsToKeep <- timeUnitsToKeep + keepPeriods
   uniqueCal <- unique(msCalFrame[,c('Year', yearSubGroup, 'DateGroup')])
   uniqueCal[,'Index'] <- seq(length(uniqueCal[,'Year']), 1, -1)
 
